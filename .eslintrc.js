98f456b81cc52ca47fe7af334ea334e760c9e870
bejow <fast.pfeil@googlemail.com>
2022-05-11 14:41:17 +0200

    Basic Setup (TS, Linter, CRA)

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  root: true,
+  plugins: ["import", "prettier"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/no-var-requires": "off",
+    "no-async-promise-executor": "off",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+      },
+    ],
+  },
+  ignorePatterns: ["*/**/lib"],
+};
