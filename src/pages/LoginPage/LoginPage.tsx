5ab01fa2b8fbf71f5628fdda31ccd2b06ca02284
ludwigschubi <ludwigschubi1@gmail.com>
2022-07-10 15:42:02 +0200

    feat: add advanced login with custom idp

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -33,36 +33,80 @@ const LoginPage: React.FC<LoginPageProps> = ({ isAuthenticating }) => {
       ) : (
         <div className={styles.main}>
           <h1>Projektor</h1>
-          <div className={styles.loginButtons}>
-            <button
-              className="login-button"
-              onClick={(e) => {
-                e.preventDefault()
-                setIsLoggingIn(true)
-                analyticsWindow.fathom?.trackGoal('GYLKIUUP', 0)
-                login({
-                  oidcIssuer: 'https://broker.pod.inrupt.com',
-                  clientName: 'Projektor Web App',
-                  clientId:
-                    process.env.NODE_ENV === 'development'
-                      ? undefined
-                      : 'https://projektor.technology/projektor.jsonld',
-                })
-              }}
-            >
-              Login with Inrupt Pod
-            </button>
-            <button
-              className="register-button"
-              onClick={(e) => {
-                e.preventDefault()
-                analyticsWindow.fathom?.trackGoal('RMPXPBWB', 0)
-                window.open('https://signup.pod.inrupt.com', '_blank')
-              }}
-            >
-              Register
-            </button>
-          </div>
+          <p>
+            This is a prototype, developed to share photos to a decentralized
+            network. The prototype protects the privacy of publishers by giving
+            them full control over the data infrastructure behind projektor. Use
+            at your own risk and enjoy.
+          </p>
+          {location.hash !== '#advanced' && (
+            <div className={styles.loginButtons}>
+              <button
+                className="login-button"
+                onClick={(e) => {
+                  e.preventDefault()
+                  setIsLoggingIn(true)
+                  analyticsWindow.fathom?.trackGoal('GYLKIUUP', 0)
+                  login({
+                    oidcIssuer: 'https://broker.pod.inrupt.com',
+                    clientName: 'Projektor Web App',
+                    clientId:
+                      process.env.NODE_ENV === 'development'
+                        ? undefined
+                        : 'https://projektor.technology/projektor.jsonld',
+                  })
+                }}
+              >
+                Login with Inrupt Pod
+              </button>
+              <button
+                className="register-button"
+                onClick={(e) => {
+                  e.preventDefault()
+                  analyticsWindow.fathom?.trackGoal('RMPXPBWB', 0)
+                  window.open('https://signup.pod.inrupt.com', '_blank')
+                }}
+              >
+                Register
+              </button>
+            </div>
+          )}
+          {location.hash !== '#advanced' && (
+            <a href="#advanced">Advanced Login</a>
+          )}
+          {location.hash === '#advanced' && (
+            <div className={styles.advanced}>
+              <form
+                title="Login with custom idp"
+                onSubmit={(e) => {
+                  e.preventDefault()
+                  const idp = (
+                    [...(e.target as HTMLFormElement).children].find(
+                      (element) => (element as HTMLInputElement).name === 'idp'
+                    ) as HTMLInputElement
+                  ).value
+                  if (confirm(`Do you really want to login to ${idp}?`)) {
+                    setIsLoggingIn(true)
+                    login({
+                      oidcIssuer: idp,
+                      clientName: 'Projektor Web App',
+                      clientId:
+                        process.env.NODE_ENV === 'development'
+                          ? undefined
+                          : 'https://projektor.technology/projektor.jsonld',
+                    })
+                  }
+                }}
+              >
+                <input
+                  name="idp"
+                  placeholder="Custom identity provider url"
+                ></input>
+                <button type="submit">Login</button>
+              </form>
+            </div>
+          )}
+          {location.hash === '#advanced' && <a href="#">Inrupt Login</a>}
         </div>
       )}
     </Page>
