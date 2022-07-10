8eb6791c49d3d37b5a4c6fb785bc9009df9192a1
ludwigschubi <ludwigschubi1@gmail.com>
2022-07-10 15:45:50 +0200

    fix: usage disclaimer

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -36,8 +36,8 @@ const LoginPage: React.FC<LoginPageProps> = ({ isAuthenticating }) => {
           <p>
             This is a prototype, developed to share photos to a decentralized
             network. The prototype protects the privacy of publishers by giving
-            them full control over the data infrastructure behind Projektor. Use
-            at your own risk and enjoy.
+            them full control over the data infrastructure behind their account.
+            Use at your own risk and enjoy.
           </p>
           {location.hash !== '#advanced' && (
             <div className={styles.loginButtons}>
