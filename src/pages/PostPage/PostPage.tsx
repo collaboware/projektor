c08f048805d0b6d649a22d156eac694842f0b529
ludwigschubi <ludwigschubi1@gmail.com>
2022-07-15 01:34:06 +0200

    fix: post page buttons

--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -104,7 +104,7 @@ const PostPage: React.FC = () => {
       <div className={styles.selectedPostWrapper}>
         {!isLoading && (
           <div className={styles.buttonBar}>
-            {renderProfileButton()}
+            {session?.info.webId !== params.webId && renderProfileButton()}
             {location.state ? (
               <button
                 onClick={(e) => {
